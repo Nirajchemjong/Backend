feae67a50a92ccf003ef2fbeac5dc0cb2f73fa46
Nirajchemjong <nirajlimbu7@gmail.com>
2023-10-19 00:52:31 +1100

    fixed environment

--- day_1/practice_ejs/index.js
+++ day_1/practice_ejs/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 3004;
-app.listen(port, () => {
-  console.log("server is running at " + port);
+const PORT = process.env.PORT || 3004;
+app.listen(PORT, () => {
+  console.log("server is running at " + PORT);
 });
